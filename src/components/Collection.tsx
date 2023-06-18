f53cbe9ae0e6e9b436ea2b0b7a536e07fe409048
abdmmar <opowae909@gmail.com>
2023-06-18 23:32:26 +0700

    add contents

--- src/components/Collection.tsx
+++ src/components/Collection.tsx
@@ -1,19 +1,59 @@
-import { getCollection } from "astro:content";
+import { CollectionEntry, getCollection } from "astro:content";
+import * as React from "react";
+import { HiArrowRight, HiArrowUpRight } from "react-icons/hi2";
 import { Masonry } from "./Masonry";
+
+type ContentProps = CollectionEntry<"blog">;
 
 const blog = await getCollection("blog");
 
 export function Collection() {
+  const items: ContentProps[] = blog;
+
+  React.useEffect(() => {
+    const url = window.location.search;
+  }, []);
+
   return (
     <Masonry breakpoints={{ 1440: 4, 960: 2, 520: 1 }}>
-      {blog.map((post) => (
-        <div
-          key={post.id}
-          className="col-span-2 rounded-md border border-gray-200 p-4 flex flex-col gap-4 w-full"
-        >
-          {post.id}
-        </div>
-      ))}
+      {items.map((post) => {
+        return (
+          <a
+            key={post.id}
+            href={`/blog/${post.slug}`}
+            className="col-span-2 rounded-md border border-gray-200 p-4 flex flex-col gap-4 w-full"
+          >
+            <div className="flex justify-between items-center w-full">
+              <div className="flex gap-5 font-ibmMono">
+                <small className="text-green-600">
+                  {post.data.tag.toUpperCase()}
+                </small>
+                <small className="text-gray-600">
+                  {new Intl.DateTimeFormat("id-ID", {
+                    dateStyle: "short",
+                  }).format(post.data.publishedAt)}
+                </small>
+              </div>
+              {post.data.tag === "Writing" ? (
+                <HiArrowRight size="0.8rem" />
+              ) : (
+                <HiArrowUpRight size="0.8rem" />
+              )}
+            </div>
+            {post.data.image ? (
+              <img
+                className="rounded-sm"
+                alt={post.data.imageAlt}
+                src={post.data.image.src}
+              />
+            ) : null}
+            <div className="flex flex-col gap-2">
+              <h3 className="text-xl">{post.data.title}</h3>
+              <p className="text-gray-500">{post.data.description}</p>
+            </div>
+          </a>
+        );
+      })}
     </Masonry>
   );
 }
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,34 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+
+interface ImageMetadata {
+  src: string;
+  width: number;
+  height: number;
+  format: string;
+}
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      tag: z.string(),
+      status: z.string(),
+      // Transform string to Date object
+      publishedAt: z
+        .string()
+        .or(z.date())
+        .transform((val) => new Date(val)),
+      updatedAt: z
+        .string()
+        .optional()
+        .transform((str) => (str ? new Date(str) : undefined)),
+      image: image().optional(),
+      imageAlt: z.string().optional(),
+      keywords: z.array(z.string()),
+      author: z.string(),
+    }),
 });
 
 export const collections = { blog };
