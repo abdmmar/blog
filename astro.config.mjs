73ed7303517ed51dc9cb20bce5956ec8babf5d73
abdmmar <opowae909@gmail.com>
2023-06-24 10:41:58 +0700

    add astro image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,19 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap(), tailwind(), react()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), image()],
   experimental: {
-    assets: true,
+    assets: true
   },
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
-    syntaxHighlight: "prism",
-  },
+    syntaxHighlight: "prism"
+  }
 });
