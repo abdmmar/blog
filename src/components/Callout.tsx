2831d8348a1866f2212f27cd0a0ffc33f4695835
abdmmar <opowae909@gmail.com>
2023-06-25 17:26:19 +0700

    fix callout and inline code on dark mode

--- src/components/Callout.tsx
+++ src/components/Callout.tsx
@@ -3,7 +3,7 @@ import { HiOutlineInformationCircle } from "react-icons/hi2";
 type CalloutProps = { children: React.ReactNode; title?: React.ReactNode };
 export default function Callout({ children, title }: CalloutProps) {
   return (
-    <div className="grid grid-cols-[min-content_1fr] items-start gap-2 p-3 pr-4 my-2 mb-4 border border-gray-200 rounded-sm bg-gray-100">
+    <div className="grid grid-cols-[min-content_1fr] items-start gap-2 p-3 pr-4 my-2 mb-4 border border-gray-200 dark:border-gray-800 rounded-sm bg-gray-100 dark:bg-gray-900">
       <div className="p-1 flex items-center">
         <HiOutlineInformationCircle className="text-blue-600 w-6 h-6" />
       </div>
