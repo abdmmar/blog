b273072b3f5eb27b47ab1ad905bc182b3e4d6f6f
abdmmar <opowae909@gmail.com>
2023-06-29 00:13:27 +0700

    fix layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,13 +7,13 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        xl: { max: "1279px" },
+        xl: { min: "1024px", max: "1279px" },
         // => @media (max-width: 1279px) { ... }
 
-        lg: { max: "1023px" },
+        lg: { min: "768px", max: "1023px" },
         // => @media (max-width: 1023px) { ... }
 
-        md: { max: "767px" },
+        md: { min: "640px", max: "767px" },
         // => @media (max-width: 767px) { ... }
 
         sm: { max: "639px" },
