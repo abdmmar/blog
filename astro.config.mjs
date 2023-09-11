c3b71b61fe4203f9348b1016d0a28f4358726610
abdmmar <opowae909@gmail.com>
2023-09-11 17:07:40 +0700

    chore: add sitemap and robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -16,6 +17,7 @@ export default defineConfig({
     mdx(),
     sitemap(),
     react(),
+    robotsTxt(),
   ],
   markdown: {
     // Can be 'shiki' (default), 'prism' or false to disable highlighting
