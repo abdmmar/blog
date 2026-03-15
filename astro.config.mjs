990c5222124f5129206913a3424db7876a2be81b
Abdullah Ammar <opowae909@gmail.com>
2026-03-15 22:58:24 +0700

    chore: add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,16 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { defineConfig } from "astro/config";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://abdmmar.com",
   integrations: [mdx(), sitemap(), react(), robotsTxt()],
+
   markdown: {
     syntaxHighlight: "prism",
   },
+
+  adapter: cloudflare(),
 });
